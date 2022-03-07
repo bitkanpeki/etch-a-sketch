84c0b14bf05e2c8ae86bb1d8084b35fb2618887d
Jonathan Boulay <jonathanboulay@outlook.com>
2022-03-08 08:55:53 +0900

    Add static grid in the canvas

---
+++ app.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector('.canvas')
+let box
+
+for (let i = 0; i < 256; i++) {
+  box = document.createElement('div')
+
+  box.addEventListener('mouseenter', (e) => {})
+  canvas.appendChild(box)
+}
