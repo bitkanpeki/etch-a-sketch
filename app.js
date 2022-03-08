887684fbc71f20e5ee2ddb79d4e6e4533b8c1522
Jonathan Boulay <jonathanboulay@outlook.com>
2022-03-08 09:50:34 +0900

    Add button to choose custom canvas size

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const canvas = document.querySelector('.canvas')
 let box
+const cells = document.querySelectorAll('.cell')
 
-for (let i = 0; i < 256; i++) {
-  box = document.createElement('div')
-
-  box.addEventListener('mouseenter', (e) => {})
-  canvas.appendChild(box)
+const handleMouseEnter = (e) => {
+  e.target.style.backgroundColor = 'blue'
 }
+
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  }
+}
+
+const generateCanvas = (canvasSize) => {
+  removeAllChildNodes(canvas)
+  canvas.style.gridTemplateColumns = `repeat(${canvasSize}, 1fr)`
+
+  for (let i = 0; i < canvasSize * canvasSize; i++) {
+    box = document.createElement('div')
+    box.classList.add('cell')
+    box.addEventListener('mouseenter', handleMouseEnter)
+    canvas.appendChild(box)
+  }
+}
+
+const changeCanvasBtn = document.querySelector('.change')
+
+changeCanvasBtn.addEventListener('click', (e) => {
+  cells.forEach((cell) => (cell.style.backgroundColor = 'aqua'))
+
+  let canvasSize = prompt('Enter the canvas size (max 100)')
+
+  if (canvasSize > 100) canvasSize = 100
+
+  generateCanvas(canvasSize)
+})
+
+generateCanvas(16)
